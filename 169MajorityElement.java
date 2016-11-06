9601bc950bcec2aae337c4ee6fbfa88107587c7a
pcoder99 <susan.2010hunter@gmail.com>
2016-11-05 22:17:43 -0700

    Create 169MajorityElement.java

---
+++ 169MajorityElement.java
@@ -0,0 +1,28 @@
+PS:
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Thought:
+  The key is why you decrement the counter
+ 
+ Implementation:
+ 
+ 
+ public int majorityElement(int[] nums) {
+        int count = 1;
+        int major = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
+            if(count == 0){
+                count++;
+                major = nums[i];
+            }else if(nums[i] == major){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        
+        return major;
+    }
